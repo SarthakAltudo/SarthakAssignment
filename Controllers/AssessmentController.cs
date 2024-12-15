0bf1ddadeff00cba3fa6174ea21090fd28c68615
Sarthak Pandey <sarthak.pandey@altudo.co>
2024-12-15 21:10:12 +0530

    commit

---
+++ Controllers/AssessmentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Mvc;
+
+namespace Sarthak.Controllers
+{
+    public class AssessmentController : Sitecore.Mvc.Controllers.SitecoreController
+
+    {
+        // GET: Assessment
+        public ActionResult Header()
+        {
+            return View();
+        }
+        public ActionResult Footer()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sarthak.MvcApplication" Language="C#" %>
